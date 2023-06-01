daf93e64420cb4d218f971e825cccb7b5da5adb3
StevenLiu <stevenliu986@gmail.com>
2023-06-01 22:08:42 +1000

    add AddStudentForm.js

--- src/js/src/forms/AddStudentForm.js
+++ src/js/src/forms/AddStudentForm.js
@@ -1,64 +1,100 @@
 import React from 'react';
 import {Formik} from "formik";
+import {Input, Button, Tag} from "antd";
+
+const inputMargin = {marginBottom: "5px"};
+const tagStyle = {backgroundColor: "#f50", color: "white", ...inputMargin};
 
 const AddStudentForm = () => {
     return (
-            <div>
-                <h1>Anywhere in your app!</h1>
-                <Formik
-                        initialValues={{email: '', password: ''}}
-                        validate={values => {
-                            const errors = {};
-                            if (!values.email) {
-                                errors.email = 'Required';
-                            } else if (
-                                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                            ) {
-                                errors.email = 'Invalid email address';
-                            }
-                            return errors;
-                        }}
-                        onSubmit={(values, {setSubmitting}) => {
-                            setTimeout(() => {
-                                alert(JSON.stringify(values, null, 2));
-                                setSubmitting(false);
-                            }, 400);
-                        }}
-                >
-                    {({
-                          values,
-                          errors,
-                          touched,
-                          handleChange,
-                          handleBlur,
-                          handleSubmit,
-                          isSubmitting,
-                          /* and other goodies */
-                      }) => (
-                            <form onSubmit={handleSubmit}>
-                                <input
-                                        type="email"
-                                        name="email"
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        value={values.email}
-                                />
-                                {errors.email && touched.email && errors.email}
-                                <input
-                                        type="password"
-                                        name="password"
-                                        onChange={handleChange}
-                                        onBlur={handleBlur}
-                                        value={values.password}
-                                />
-                                {errors.password && touched.password && errors.password}
-                                <button type="submit" disabled={isSubmitting}>
-                                    Submit
-                                </button>
-                            </form>
-                    )}
-                </Formik>
-            </div>
+            <Formik
+                    initialValues={{firstName: '', lastName: '', email: '', gender: ''}}
+                    validate={values => {
+                        const errors = {};
+                        if (!values.firstName) {
+                            errors.firstName = "First Name Required"
+                        }
+                        if (!values.lastName) {
+                            errors.lastName = "Last Name Required"
+                        }
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (
+                                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        ) {
+                            errors.email = 'Invalid email address';
+                        }
+                        if (!values.gender) {
+                            errors.gender = "Gender Required"
+                        } else if (!['FEMALE', 'MALE', 'female', 'male'].includes(values.gender)) {
+                            errors.gender = "Gender must be (MALE, male, FEMALE, female)"
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, {setSubmitting}) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+            >
+                {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit,
+                      isSubmitting,
+                      submitForm,
+                      isValid
+                      /* and other goodies */
+                  }) => (
+                        <form onSubmit={handleSubmit}>
+                            <Input
+                                    style={inputMargin}
+                                    name="firstName"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.firstName}
+                                    placeholder="First name eg: John"
+                            />
+                            {errors.firstName && touched.firstName && <Tag style={tagStyle}>{errors.firstName}</Tag>}
+                            <Input
+                                    style={inputMargin}
+                                    name="lastName"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.lastName}
+                                    placeholder="Last name eg: Smith"
+                            />
+                            {errors.lastName && touched.lastName && <Tag style={tagStyle}>{errors.lastName}</Tag>}
+                            <Input
+                                    style={inputMargin}
+                                    type="email"
+                                    name="email"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.email}
+                                    placeholder="Email eg: example@example.com"
+                            />
+                            {errors.email && touched.email && <Tag style={tagStyle}>{errors.email}</Tag>}
+                            <Input
+                                    style={inputMargin}
+                                    name="gender"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.gender}
+                                    placeholder="Female or Male"
+                            />
+                            {errors.gender && touched.gender && <Tag style={tagStyle}>{errors.gender}</Tag>}
+                            <Button onClick={() => submitForm()} type="submit"
+                                    disabled={isSubmitting || (touched && !isValid)}>
+                                Submit
+                            </Button>
+                        </form>
+                )}
+            </Formik>
     );
 }
 
