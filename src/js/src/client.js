b4473a7285882a17131d6b6c4d5eae124d2f6884
StevenLiu <stevenliu986@gmail.com>
2023-06-03 12:16:18 +1000

    add method to insert new student

--- src/js/src/client.js
+++ src/js/src/client.js
@@ -1,3 +1,8 @@
 import fetch from "unfetch";
 
 export const getAllStudents = () => fetch("http://localhost:8080/api/students");
+export const addNewStudent = student => fetch("http://localhost:8080/api/students", {
+    headers: {"Content-Type": "application/json"},
+    method: "POST",
+    body: JSON.stringify(student)
+})
