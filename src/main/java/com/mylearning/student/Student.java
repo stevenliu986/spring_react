6f0a43a08fccf036830ac725b5d74c81d7a6854b
StevenLiu <stevenliu986@gmail.com>
2023-06-03 12:17:15 +1000

    update Student class's constructor

--- src/main/java/com/mylearning/student/Student.java
+++ src/main/java/com/mylearning/student/Student.java
@@ -1,5 +1,7 @@
 package com.mylearning.student;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 /**
@@ -18,11 +20,11 @@ public class Student {
     private final String email;
     private final Gender gender;
 
-    public enum Gender{
+    public enum Gender {
         MALE, FEMALE
     }
 
-    public Student(UUID studentId, String firstName, String lastName, String email, Gender gender) {
+    public Student(@JsonProperty("studentId") UUID studentId, @JsonProperty("firstName") String firstName, @JsonProperty("lastName") String lastName, @JsonProperty("email") String email, @JsonProperty("gender") Gender gender) {
         this.studentId = studentId;
         this.firstName = firstName;
         this.lastName = lastName;
