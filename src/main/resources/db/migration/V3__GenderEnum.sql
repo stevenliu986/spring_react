f010636b773754f8546d54635d0f5c29685e538e
StevenLiu <stevenliu986@gmail.com>
2023-06-10 09:17:34 +1000

    add gender enum

--- src/main/resources/db/migration/V3__GenderEnum.sql
+++ src/main/resources/db/migration/V3__GenderEnum.sql
@@ -1,5 +1,5 @@
 -- 创建gender enum
-create type gender as enum ('MALE', 'FEMALE');
+-- create type gender as enum ('MALE', 'FEMALE');
 
 -- 修改student中的gender类型为enum
-alter table student alter column gender type gender;
+alter table student alter column gender type gender using (gender::gender);
